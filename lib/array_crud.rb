54c014b9713fda652dc885614a9505728fe80631
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-18 18:41:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+   t_family = ["Zoe", "Nika", "Eugene", "arrays!"]
+   arrays = t_family.pop
 end
 
 def remove_element_from_start_of_array(array)
