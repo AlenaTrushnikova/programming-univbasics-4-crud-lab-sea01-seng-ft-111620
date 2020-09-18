35d18c41a4a41597f17f9652da6ec3f20346c119
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-18 18:45:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,6 +43,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  t_family = ["Zoe", "Nika", "Eugene", "arrays!"]
-  t_family[-1]
+  t_family = ["Zoe", "Nika", "Eugene", "Alena"]
+  t_family[4] = "totally"
 end
